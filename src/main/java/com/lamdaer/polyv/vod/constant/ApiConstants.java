da1292c72304c02d613bef01734b7358c3e4a79b
huangzijian <huangzijian@lamdaer.com>
2020-05-04 02:11:46 +0800

    接口地址

---
+++ src/main/java/com/lamdaer/polyv/vod/constant/ApiConstants.java
@@ -0,0 +1,19 @@
+package com.lamdaer.polyv.vod.constant;
+
+/**
+ * 点播接口常量类
+ *
+ * @author lamdaer
+ * createTime 2020/05/03
+ */
+public class ApiConstants {
+
+    /**
+     * 获取用户空间及流量情况
+     */
+    public static final String GET_USER_SPACE_TRAFFIC_USAGE = "http://api.polyv.net/v2/user/{userid}/main";
+
+    private ApiConstants() {
+    }
+
+}
