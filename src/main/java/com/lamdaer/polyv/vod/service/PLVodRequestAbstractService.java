a91d1ef7f023b7b887d83169979e8219f0106c5e
huangzijian <huangzijian@lamdaer.com>
2020-05-14 00:00:08 +0800

    添加post方法

--- src/main/java/com/lamdaer/polyv/vod/service/PLVodRequestAbstractService.java
+++ src/main/java/com/lamdaer/polyv/vod/service/PLVodRequestAbstractService.java
@@ -1,5 +1,7 @@
 package com.lamdaer.polyv.vod.service;
 
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpStatus;
 import cn.hutool.http.HttpUtil;
 import cn.hutool.json.JSON;
 import cn.hutool.json.JSONUtil;
@@ -24,11 +26,33 @@ public abstract class PLVodRequestAbstractService {
      */
     protected PLVodCommonResult getRequest(String url, Map<String, Object> params) {
         String result = HttpUtil.get(url, params);
+        return getPLVodCommonResult(result);
+    }
+
+    /**
+     * post 请求
+     *
+     * @param url    接口地址
+     * @param params 请求参数
+     * @return 响应结果
+     */
+    protected PLVodCommonResult postRequest(String url, Map<String, Object> params) {
+        String result = HttpRequest.post(url).form(params).execute().body();
+        return getPLVodCommonResult(result);
+    }
+
+    /**
+     * 返回信息处理
+     *
+     * @param result 原始返回信息
+     * @return 处理后的返回信息
+     */
+    private PLVodCommonResult getPLVodCommonResult(String result) {
         JSON jsonResult = JSONUtil.parse(result);
         int code = (int) jsonResult.getByPath("code");
         String status = (String) jsonResult.getByPath("status");
         String message = (String) jsonResult.getByPath("message");
-        if (200 == code) {
+        if (HttpStatus.HTTP_OK == code) {
             return new PLVodCommonResult(code, status, message, jsonResult.getByPath("data"));
         }
         return new PLVodCommonResult(code, status, message, null);
